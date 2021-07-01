e8036ebc64a110b62fa015a2b82bccdb135343fe
Xiangdong Zeng <xdzeng96@gmail.com>
2021-07-01 21:36:21 +0800

    update special-paste.py

--- special-paste.py
+++ special-paste.py
@@ -16,7 +16,6 @@ typeName = pasteboard.availableTypeFromArray_(["Glyphs elements pasteboard type"
 if typeName == "Glyphs elements pasteboard type":
     data = pasteboard.dataForType_(typeName)
     dictionary = NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding).propertyList()
-    # Glyphs 3
     if "shapes" in dictionary:
         elements = dictionary["shapes"]
         for shapeDict in elements:
@@ -27,16 +26,21 @@ if typeName == "Glyphs elements pasteboard type":
                 shape = GSComponent.alloc().initWithDict_format_(shapeDict, GSFormatVersion)
             for layer in Glyphs.font.selectedLayers:
                 layer.shapes.append(shape)
-    # Glyphs 2
     if "paths" in dictionary:
         elements = dictionary["paths"]
         for pathDict in elements:
-            path = GSPath.alloc().initWithPathDict_(pathDict)
+            if Glyphs.font.formatVersion == 3:
+                path = GSPath.alloc().initWithDict_format_(pathDict, GSFormatVersion)
+            else:
+                path = GSPath.alloc().initWithPathDict_(pathDict)
             for layer in Glyphs.font.selectedLayers:
                     layer.paths.append(path)
     if "components" in dictionary:
         elements = dictionary["components"]
         for componentDict in elements:
-            component = GSComponent.alloc().initWithElementDict_(componentDict)
+            if Glyphs.font.formatVersion == 3:
+                component = GSComponent.alloc().initWithDict_format_(componentDict, GSFormatVersion)
+            else:
+                component = GSComponent.alloc().initWithElementDict_(componentDict)
             for layer in Glyphs.font.selectedLayers:
                     layer.components.append(component)
